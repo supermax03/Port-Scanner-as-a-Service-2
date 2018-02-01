116af2669640394daa9363053b8a175441b4df0c
supermax03 <maximiliano.bordon@gmail.com>
2018-01-31 23:43:48 -0300

    add executor

---
+++ processor/executor.py
@@ -0,0 +1,15 @@
+from dao.dataaccesslayer import *
+from dao.entities import *
+from json import dumps,JSONEncoder
+import datetime
+
+def getstatus(hash):
+        op=dal.Session.query(Operation).filter(Operation.hash==hash).first()
+        print(datetime.datetime.now())
+        time=(datetime.datetime.now()-op.updated_on).total_seconds()
+        print(time)
+        if (time<=3600):
+            results={'state':op.state,'results':op.results,'date':str(op.updated_on)}
+        else:
+            results={'state':'No longer Available'}
+        return results
