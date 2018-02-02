af75839b47b416adc1ef15dbd3b10923199d6cde
supermax03 <maximiliano.bordon@gmail.com>
2018-02-02 02:10:05 -0300

    add processor

--- processor/executor.py
+++ processor/executor.py
@@ -1,7 +1,9 @@
 from dao.dataaccesslayer import *
 from dao.entities import *
-from myutils.utils import isexpiredtoken
+from myutils.utils import isexpiredtoken, gethash
+from processor import sender
 import sys
+import json
 
 
 def getstatus(hash):
@@ -9,12 +11,43 @@ def getstatus(hash):
         op = dal.Session.query(Operation).filter(Operation.hash == hash).first()
         if (op):
             if not isexpiredtoken(op.updated_on):
-                results = {'state': op.state, 'results': op.results, 'date': str(op.updated_on), 'code': 200}
+                results = {"state": op.state,
+                           "results": op.results,
+                           "date": str(op.updated_on),
+                           "code": 200}
             else:
-                results = {'state': 'No longer Available', 'code': 200}
+                results = {"state": "No longer Available",
+                           "code": 200}
         else:
-            results = {'state': 'Not found', 'code': 404}
+            results = {"state": "Not found",
+                       "code": 404}
     except:
-        results = {'state': 'Internal server Error', 'exception': str(sys.exc_info()[1]), 'code': 500}
+        results = {"state": "Internal server Error",
+                   "exception": str(sys.exc_info()[1]), "code": 500}
+    finally:
+        return results
+
+
+def validateop(op):
+    task = dal.Session.query(Task).filter(Task.name == op['operation'])
+    return (task != None)
+
+
+def addOperation(op):
+    try:
+
+        if (validateop(op)):
+            op = [op]
+            op[0]['idreq'] = gethash()
+            msg = json.dumps(op)
+            sender.send(msg)
+            results = {"state": 'Operacion Valida',
+                       "code": 200,
+                       "yourid": op[0]['idreq'],
+                       "payload": op[0]['payload'],
+                       "operation": op[0]['operation']}
+
+    except:
+        print(sys.exc_info())
     finally:
         return results
--- processor/sender.py
+++ processor/sender.py
@@ -1,15 +1,13 @@
 import pika
-from myutils.utils import gethash
 
 
-def send():
-    hash = gethash()
+def send(msg):
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
     channel.queue_declare(queue='spy')
     channel.basic_publish(exchange='',
                           routing_key='spy',
-                          body=hash)
+                          body=msg)
     print(" [x] Sent 'hash'")
     connection.close()
     return hash
